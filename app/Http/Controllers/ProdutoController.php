7c4fcac60fa3fb74d5a4ec36730fc922013e4b10
JJgustavoJJ <gustavogaldino820@gmail.com>
2026-01-22 13:56:30 -0300

    ATUALIZAÇÃO

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Produto;
+
+class ProdutoController extends Controller
+{
+    public function store (Request $request){
+$produto = Produto::create([
+    'marca'=>$request->marca,
+    'descricao'=>$request->descricao,
+    'valor_unitario'=>$request->valor_unitario,
+    'quantidade_estoque'=>$request->quantidade_estoque,
+    'faixa_etaria_minima'=>$request->faixa_etaria_minima
+]);
+    }
+    public function index(){
+        $produto = Produto::all();
+        return response()->json($produto);
+    }
+
+
+  public function update(Request $request, $id)
+    {
+        $produto = Produto::find($id);
+        if (!$produto) {
+
+            return response()->json(['mensagem' => 'atualizado com sucesso']);
+            $produto->update();
+        }
+        return response()->json([$produto]);
+    }
+
+  public function delete($id)
+    {
+        $produto = Produto::find($id);
+
+        if (!$produto) {
+            return response()->json(["mensagem" => 'ID não encontrado']);
+        }if(isset($request->id)){
+$produto->$id = $request->id;
+        }
+         if(isset($request->marca)){
+$produto->marca = $request->marca;
+        }
+         if(isset($request->id)){
+$produto->descricao = $request->id;
+        }
+         if(isset($request->id)){
+$produto->valor_unitario = $request->id;
+        }
+        if(isset($request->id)){
+$produto->quantidade_estoque = $request->id;
+        }
+         if(isset($request->id)){
+$produto->faixa_etaria_minima = $request->id;
+        }
+
+
+        $produto->delete();
+
+        return response()->json(["mensagem" => 'deletado com sucesso']);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProdutoController;
+use App\Models\Produto;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::PUT('/Produto',[ProdutoController::class,'update']);
+Route::GET('/Produto',[ProdutoController::class, 'index' ]);
+Route::DELETE('/Produto',[ProdutoController::class, 'delete' ]);
+Route::POST('/Produto',[ProdutoController::class, 'store' ]);
