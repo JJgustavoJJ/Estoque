6088502b5604066008fa78cbcc9eaa0eddb46c79
JJgustavoJJ <gustavogaldino820@gmail.com>
2026-01-27 15:47:03 -0300

    Atualização

--- app/Http/Controllers/EntradaController.php
+++ app/Http/Controllers/EntradaController.php
@@ -4,31 +4,46 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Entrada;
+use App\Models\Produto;
 
 class EntradaController extends Controller
 {
     public function store(Request $request){
-        $entrada = Entrada::create([
+        $produto = Produto::find($request->id_produto);
+        if($produto == null){
+            return response()->json([
+                'mensagem' => 'não encontrado'
+            ]);
+            $entrada = Entrada::create([
             'id_produto'=>$request->id_produto,
             'quantidade'=>$request->quantidade
         ]);
-        return response()->json($entrada);
+        if(isset($request->quantidade)){
+            $produto->quantidade_estoque += $request->quantidade;
+        }
+        $produto->update();
+        return response()->json('Atualizado');
     }
+}
 
     public function index(){
         $entrada = Entrada::all();
         return response()->json($entrada);
     }
 
-    public function delete($id){
+    public function delete(Request $request, $id){
         $entrada = Entrada::find($id);
-        if($entrada == null){
-             return response()->json(["mensagem" => 'ID não encontrado']);
+        $produto = Produto::find($request->id_produto);
+        if($produto == null){
+            return response()->json([
+                'mensagem' => 'não encontrado'
+            ]);
+     if(isset($request->quantidade)){
+            $produto->quantidade_estoque -= $request->quantidade;
         }
-       $entrada->delete();
-       return response()->json([
-        "mensagem" => 'Produto deletado'
-       ]);
+         $produto->update();
+        return response()->json('Atualizado');
+    }
     }
 
 }
